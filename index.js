db26231f6505cfec38500d82998345eaa6c81c4f
shivamm-mehta <mehtas25@mymacewan.ca>
2024-02-29 16:57:46 -0700

    Update the file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-for (let i=0 ; i<=200; i++){
-    if(i %2 ==0){
-        console.log(`${i} is even`);
+function checkNumbers() {
+  for (let i = 0; i <= 200; i++) {
+    if (i % 2 == 0) {
+      console.log(`${i} is even`);
+    } else {
+      console.log(`${i} is odd`);
     }
-    else{
-        console.log(`${i} is odd`)
-    }
+  }
 }
+checkNumbers();
